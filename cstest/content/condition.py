c5e6bdc9f2fc6db2ae7c0893b1a44ae4b82fb5e6
Philip <p-sinfield@live.co.uk>
2023-03-21 01:48:17 +0000

    Continued improvements to condition parser

--- cstest/content/condition.py
+++ cstest/content/condition.py
@@ -98,21 +98,13 @@ def split_list(lst: list) -> Tuple[list[Any]]:
     return lst[0], lst[1], lst[2]  # type: ignore
 
 
-1 or [[2, 3]]
-1 or [[2], [3]]
-[[1, 2]] or [[3, 4]]
-[[1, 2]] or [[3], [4]]
-[[1], [2]] or [[3, 4]]
-[[1], [2]] or [[3], [4]]
-
-
 def parse_condition_element(lst: list[Any]) -> list[list[int]]:
     if not isinstance(lst[0], list) and not isinstance(lst[2], list):
         return [[lst[0], lst[2]]] if lst[1] == "AND" else [[lst[0]], [lst[2]]]
     elif isinstance(lst[0], list) and not isinstance(lst[2], list):
         if lst[1] == "AND":
             for new_list in lst[0]:
-                new_list.extend(lst[2])
+                new_list.extend([lst[2]])
 
     elif isinstance(lst[0], list) and isinstance(lst[2], list):
         if lst[1] == "AND":
@@ -128,7 +120,8 @@ def parse_condition_element(lst: list[Any]) -> list[list[int]]:
     elif not isinstance(lst[0], list) and isinstance(lst[2], list):
         if lst[1] == "AND":
             for new_list in lst[2]:
-                new_list.extend(lst[0])
+                new_list.extend([lst[0]])
+        return [new_list]
 
     return lst
 
@@ -145,7 +138,7 @@ def iterate_list(lst: list[Any], indexer: list[int]) -> Tuple[list[Any], list[in
 
 
 def unpack_list(lst: list[Any]) -> list[list[int]]:
-    while has_sublist(lst):
+    for x in range(5):  # while has_sublist(lst[0]) or has_sublist(lst[2]):
         alteration, indexer = iterate_list(lst, [])
 
         statement = "lst"
@@ -155,16 +148,12 @@ def unpack_list(lst: list[Any]) -> list[list[int]]:
 
         exec(statement)
 
-        print(lst)
-
-    # break
-
-    return 0  # type: ignore
+    return parse_condition_element(lst)
 
 
 def create_condition_map(
     string_conditions: dict[int, Condition], list_string: list[str]
-) -> dict[int, list[Condition]]:
+) -> list[list[int]]:
     condition_string = "["
     cond_num = 1
 
@@ -177,52 +166,13 @@ def create_condition_map(
 
     condition_string += "]"
 
-    # lst = ast.literal_eval(condition_string)
+    lst = ast.literal_eval(condition_string)
 
-    # left, operator, right = split_list(lst)
+    if not has_sublist(lst):
+        [lst] = lst
 
-    # unpack_list(left)
+    return unpack_list(lst)
 
-    return 0  # type: ignore
-
-
-conditions = [
-    [1, 2, 4, 5, 10],
-    [1, 3, 4, 5, 10],
-    [1, 2, 6, 10],
-    [1, 3, 6, 10],
-    [7, 8, 10],
-    [9, 10],
-]
-
-
-[
-    [
-        [
-            [
-                [
-                    1,
-                    "AND",
-                    [2, "OR", 3],
-                ],
-                "AND",
-                [[4, "AND", 5], "OR", 6],
-            ],
-            "OR",
-            [7, "AND", 8],
-        ],
-        "OR",
-        9,
-    ],
-    "AND",
-    10,
-]
-
-
-# "*if ((var_1 or var_2 = 20) and var_3 >= 12) or (var_4 = 'lala' and not(var_5))",
-
-# *if ((((a and (b or c)) and ((d and e) or f)) or (g and h)) or i) and j
-#     1234      5      43     45       4     32    3       21     0
 
 strings = [
     # "(bailey_available or tommy_available) or lopez_available",
@@ -240,6 +190,7 @@ strings = [
     # "(bailey_available or tommy_available)",
     # "*if ((((a and (b or c)) and ((d and e) or f)) or (g and h)) or i) and j"
     "*if ((((a and (b and c)) and ((d and e) and f)) and (g and h)) and i) and j"
+    # "*if (a and (b and (c and d)))"
 ]
 
 
@@ -260,89 +211,5 @@ for string in strings:
         string_conditions = parse_conditions(list_string)
         condition_map = create_condition_map(string_conditions, list_string)
 
-    # print(string_conditions)
-    # print(condition_map)
-
-"""
-CAN THERE BE LOTS OF NESTED BRACKETS - I.E., NOT 2 DEEP
-*if (a and (b or (c and (d or e))))
-
-
-OR opposes the two things in brackets
-OR outside of brackets opposes the objects on the right to the objects on the left
-
-AND concats the two things in brackets
-AND outside of brackets concats the objects on the right with the objects on the left
-
-Brackets around single condition can be removed/ignored
-"""
-
-# Something like - take a condition chain at a depth (base). Then for a lower depth,
-# work out the combinations, then append them all to the base condition
-
-"""
-*if ((((a and (b or c)) and ((d and e) or f)) or (g and h)) or i) and j
-              xxxxxxxx       ---------           ---------
-       ----------------     xxxxxxxxxxxxxxxx
-      ---------------------------------------
-     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    -------------------------------------------------------------------
-
-depth 1 - Search for conditions at same depth
-depth 2 - Search for conditions at same depth
-depth 3 - Search for conditions at same depth
-depth 4 - Search for conditions at same depth
-Cond_A - depth 4
-AND - Stash condition and search for conditions at same or lower depth
-depth 5 - Search for conditions at same depth
-Cond_B  - depth 5
-OR - Stash condition and search for opposed pair
-Cond_C - depth 5
-Build array of opposed pair
-Depth 4 - Continue search
-Depth 3 - build condition group of Concat + opposed pair - depth 3
-AND - Stash all condition groups and search for conditions of same or lower depth
-
-
-*if (a and (((b and c) or (d and e)) or f))
-             ---------    ---------
-            xxxxxxxxxxxxxxxxxxxxxxxx
-           xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    ---------------------------------------
-     have_cond_a - depth 1
-     cond_a + xxx - depth 1 onwards
-     depth 2
-     add cond_a - depth 1
-     depth 3
-     depth 4
-     have_cond_b - depth 4
-     cond_b + xxx - depth 4 onwards
-     have_cond_c - depth 4
-     cond_c + xxx - depth 4 onwards
-     depth 3
-     add cond_b - depth 4
-     add cond_c - depth 4
-     Oppose - Anything depth 4+
-     have_cond_d - depth 4
-     cond_d + xxx - depth 4 onwards
-     have_cond_e - depth 4
-     cond_e + xxx - depth 4 onwards
-     depth 3
-     add cond_d - depth 4
-     add cond_e - depth 4
-     depth 2
-     Oppose - Anything depth 2 +
-
-
-
-
-*if (a or (((b and c) or (d and e)) or f))
-            ---------    ---------
-           xxxxxxxxxxxxxxxxxxxxxxxx
-          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-
-*if (a and (b or (c and (d or e))))
-"""
+    print(string_conditions)
+    print(condition_map)
