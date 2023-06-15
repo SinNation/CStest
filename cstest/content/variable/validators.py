9e786567e3ca04b1b2e3a378f646bbf52c0b1e9f
Philip <p-sinfield@live.co.uk>
2023-06-15 13:50:15 +0100

    Converted validators to class

--- cstest/content/variable/validators.py
+++ cstest/content/variable/validators.py
@@ -1,86 +1,62 @@
+from dataclasses import dataclass, field
 from typing import Any
 
 from cstest.content.errors.error import var_error_string
+from cstest.content.variable.constants import INVALID_VAR_SYMBOLS
 from cstest.content.variable.exceptions import NoVariableNameValue
 
-INVALID_VAR_SYMBOLS = [
-    "'",
-    '"',
-    "!",
-    "£",
-    "$",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "-",
-    "=",
-    "+",
-    "{",
-    "}",
-    ":",
-    ";",
-    "@",
-    "~",
-    "#",
-    "\\",
-    "|",
-    ",",
-    "<",
-    ".",
-    ">",
-    "/",
-    "?",
-    " ",
-]
 
+@dataclass
+class CallVarValidator:
+    """Variable name structure validator class for called variables."""
 
-def empty_variable_name(name: str) -> None:
-    if name == "" or str(name).isspace():
-        raise NoVariableNameValue("Error in CStest: Identified variable name is blank")
+    name: str
+    game_variables: list[str] = field(default_factory=list)
 
+    def is_blank(self) -> None:
+        """Raises exception if the passed variable name is blank.
+        This is a code error if it occurs"""
+        if self.name == "" or str(self.name).isspace():
+            raise NoVariableNameValue(
+                "Error in CStest: Identified variable name is blank"
+            )
 
-def is_first_alpha(name: str) -> bool:
-    """Checks the variable name begins with an alpha character"""
-    return True if str(name)[0].isalpha() else False
+    def is_first_alpha(self) -> bool:
+        """Checks the variable name begins with an alpha character"""
+        return True if str(self.name)[0].isalpha() else False
 
+    def inv_symbol(self) -> bool:
+        """Checks the variable name doesn't contain an invalid symbol"""
+        return (
+            True if [sym for sym in INVALID_VAR_SYMBOLS if sym in self.name] else False
+        )
 
-def inv_symbol(name: str) -> bool:
-    """Checks the variable name doesn't contain an invalid symbol"""
-    return True if not [sym for sym in INVALID_VAR_SYMBOLS if sym in name] else False
+    def is_name_valid(self) -> list[str]:
+        """Checks the variable name passes all required checks"""
+        errors: list = []
+        if not self.is_first_alpha():
+            errors.append(var_error_string("first_alpha", self.name))
+        if self.inv_symbol():
+            errors.append(var_error_string("inv_symbol", self.name))
 
+        return errors
 
-def validate_variable_name(name: str) -> list[str]:
-    """Checks the variable name passes all required checks"""
-    errors: list = []
-    if not is_first_alpha(name):
-        errors.append(var_error_string("first_alpha", name))
-    if not inv_symbol(name):
-        errors.append(var_error_string("inv_symbol", name))
+    def single_hash(self) -> bool:
+        """Checks there is only a"""
+        return True if self.name.count("#") == 1 else False
 
-    return errors
+    def hash_is_number(self) -> bool:
+        return True if str(self.name).isnumeric() else False
 
+    def is_first_bracket(self) -> bool:
+        """Checks if the first character of a variable name is a square bracket"""
+        return True if self.name[0] == "[" else False
 
-def first_char_sq_bracket(name: str) -> bool:
-    """Checks if the first characer of a variable name is a square bracket"""
-    return True if name[0] == "[" else False
+    def is_equal_bracket(self) -> bool:
+        """Checks the full variable name has an  equal number
+        of open and closed square brackets"""
+        return True if self.name.count("[") == self.name.count("]") else False
 
-
-def equal_sq_brackets(name: str) -> bool:
-    """Checks the full variable name has an  equal number
-    of open and closed square brackets"""
-    return True if name.count("[") == name.count("]") else False
-
-
-def is_defined_variable(name: str, game_variables: list[str]) -> bool:
-    """Checks that the called variable name is defined in the game"""
-    return True if name in game_variables else False
-
-
-def check_count_hashes(name: str) -> bool:
-    return True if name.count("#") == 1 else False
-
-
-def check_hash_is_number(hash: Any) -> bool:
-    return True if str(hash).isnumeric() else False
+    def is_defined(self) -> bool:
+        """Checks that the called variable name is defined in the game"""
+        return True if self.name in self.game_variables else False
--- tests/content/variable/test_validators.py
+++ tests/content/variable/test_validators.py
@@ -1,7 +1,7 @@
 import pytest
 
-from cstest.content.variable import validators as v
 from cstest.content.variable.exceptions import NoVariableNameValue
+from cstest.content.variable.validators import CallVarValidator
 
 
 @pytest.mark.parametrize(
@@ -10,7 +10,7 @@ from cstest.content.variable.exceptions import NoVariableNameValue
 )
 def test_empty_variable_nname(name: str) -> None:
     with pytest.raises(NoVariableNameValue):
-        v.empty_variable_name(name)
+        CallVarValidator(name).is_blank()
 
 
 @pytest.mark.parametrize(
@@ -25,22 +25,22 @@ def test_empty_variable_nname(name: str) -> None:
     ],
 )
 def test_is_first_alpha(name: str, exp_outcome: bool) -> None:
-    assert v.is_first_alpha(name) == exp_outcome
+    assert CallVarValidator(name).is_first_alpha() == exp_outcome
 
 
 @pytest.mark.parametrize(
     "name, exp_outcome",
     [
-        ("name", True),
-        ("valid_symbol[name]", True),
-        ("£", False),
-        ("Wo=rd", False),
-        ("bla nk", False),
-        ("Bl@$h", False),
+        ("name", False),
+        ("valid_symbol[name]", False),
+        ("£", True),
+        ("Wo=rd", True),
+        ("bla nk", True),
+        ("Bl@$h", True),
     ],
 )
 def test_inv_symbol(name: str, exp_outcome: bool) -> None:
-    assert v.inv_symbol(name) == exp_outcome
+    assert CallVarValidator(name).inv_symbol() == exp_outcome
 
 
 @pytest.mark.parametrize(
@@ -60,12 +60,12 @@ def test_inv_symbol(name: str, exp_outcome: bool) -> None:
     ],
 )
 def test_validate_variable_name(name: str, exp_error: str) -> None:
-    assert v.validate_variable_name(name) == exp_error
+    assert CallVarValidator(name).is_name_valid() == exp_error
 
 
 @pytest.mark.parametrize("name, exp_outcome", [("name", False), ("[name", True)])
 def test_first_char_sq_bracket(name: str, exp_outcome: bool) -> None:
-    assert v.first_char_sq_bracket(name) == exp_outcome
+    assert CallVarValidator(name).is_first_bracket() == exp_outcome
 
 
 @pytest.mark.parametrize(
@@ -80,7 +80,7 @@ def test_first_char_sq_bracket(name: str, exp_outcome: bool) -> None:
     ],
 )
 def test_equal_sq_brackets(variables: str, exp_outcome: bool) -> None:
-    assert v.equal_sq_brackets(variables) == exp_outcome
+    assert CallVarValidator(variables).is_equal_bracket() == exp_outcome
 
 
 @pytest.mark.parametrize(
@@ -94,7 +94,7 @@ def test_equal_sq_brackets(variables: str, exp_outcome: bool) -> None:
 def test_is_defined_variable(
     name: str, game_variables: list[str], exp_outcome: bool
 ) -> None:
-    assert v.is_defined_variable(name, game_variables) == exp_outcome
+    assert CallVarValidator(name, game_variables).is_defined() == exp_outcome
 
 
 @pytest.mark.parametrize(
@@ -102,7 +102,7 @@ def test_is_defined_variable(
     [("var1", False), ("var1#1", True), ("var1#1#2", False), ("v#ar#1#2", False)],
 )
 def test_check_count_hashes(name: str, exp_outcome: bool) -> None:
-    assert v.check_count_hashes(name) == exp_outcome
+    assert CallVarValidator(name).single_hash() == exp_outcome
 
 
 @pytest.mark.parametrize(
@@ -117,4 +117,4 @@ def test_check_count_hashes(name: str, exp_outcome: bool) -> None:
     ],
 )
 def test_check_hash_is_number(name: str, exp_outcome: bool) -> None:
-    assert v.check_hash_is_number(name) == exp_outcome
+    assert CallVarValidator(name).hash_is_number() == exp_outcome
